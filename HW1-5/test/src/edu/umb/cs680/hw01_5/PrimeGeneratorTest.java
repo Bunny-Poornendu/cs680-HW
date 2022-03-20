119e2d05bf1fe9d762451bfae1da6440f5669cfc
Bunny-Poornendu <85818734+Bunny-Poornendu@users.noreply.github.com>
2022-03-20 13:48:07 -0400

    Add files via upload

---
+++ HW1-5/test/src/edu/umb/cs680/hw01_5/PrimeGeneratorTest.java
@@ -0,0 +1,50 @@
+package edu.umb.cs680.hw01_5;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class PrimeGeneratorTest {
+
+    @Test
+    public void Primegeneratorfrom1to10()
+    {
+        PrimeGenerator gen = new PrimeGenerator(1, 10);
+        gen.generatePrimes();
+        Long[] expectedPrimes = {2L, 3L, 5L, 7L};
+        assertArrayEquals(expectedPrimes, gen.getPrimes().toArray());
+    }
+
+    @Test
+    public void Primegeneratorfrom10to1runtimeexception()
+    {
+        try {
+            PrimeGenerator PG = new PrimeGenerator(10,1);
+        }
+        catch (RuntimeException exception) {
+            assertEquals("Wrong input values: from=10 to=1", exception.getMessage());
+        }
+
+    }
+    @Test
+    public void Primegeneratorfrom20to30()
+    {
+        PrimeGenerator gen = new PrimeGenerator(20, 30);
+        gen.generatePrimes();
+        Long[] expectedPrimes = {23L, 29L};
+        assertArrayEquals(expectedPrimes, gen.getPrimes().toArray());
+    }
+
+    @Test
+    public void Primegeneratorfrom100to1runtimeexception()
+    {
+        try {
+            PrimeGenerator PG = new PrimeGenerator(100,1);
+        }
+        catch (RuntimeException exception) {
+            assertEquals("Wrong input values: from=100 to=1", exception.getMessage());
+        }
+
+    }
+
+
+}
