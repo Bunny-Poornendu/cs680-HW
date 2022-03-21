52f75b4aa6a196dc8abf173da48044cd1a208dd3
Bunny-Poornendu <85818734+Bunny-Poornendu@users.noreply.github.com>
2022-03-20 20:14:31 -0400

    Add files via upload

--- HW2/src/edu/umb/cs680/hw02/Singleton.java
+++ HW2/src/edu/umb/cs680/hw02/Singleton.java
@@ -1,14 +1,19 @@
 package edu.umb.cs680.hw02;
 
 public class Singleton {
-	private Singleton() {};
-	private static Singleton instance = null;
-	
-	public static Singleton getInstance() {
-		if(instance == null) {
-			instance = new Singleton();
-		}
-		return instance;
-	}
+    private Singleton() {};
+    private static Singleton instance = null;
 
+    public static Singleton getInstance() {
+        if (instance == null)
+            instance = new Singleton();
+        return instance;
+    }
+    public static void main(String[] args){
+	Singleton sin = Singleton.getInstance();
+	System.out.println(sin.hashCode());
+	}
+	
 }
+
+
--- HW2/test/src/edu/umb/cs680/hw02/SingletonTest.java
+++ HW2/test/src/edu/umb/cs680/hw02/SingletonTest.java
@@ -1,24 +1,24 @@
 package edu.umb.cs680.hw02;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import edu.umb.cs680.hw02.Singleton;
-
 
 
 public class SingletonTest {
 
-	@Test
-	public void getInstanceReturnNonNullValue() {
-		assertNotNull(Singleton.getInstance());
-	}
-	
-	@Test
-	public void getInstanceReturnIdenticalInstance() {
-		Singleton s1 = Singleton.getInstance();
-		Singleton s2 = Singleton.getInstance();
-		assertSame(s1, s2);
-	}
+    @Test
+    public void getinstanceforNonNullValue()
+    {
+        Singleton sin = Singleton.getInstance();
+        Assertions.assertNotNull(sin);
+    }
+
+    @Test
+    public void getinstanceforidenticalinstancesmultipletimes()
+    {
+        Singleton sin1 = Singleton.getInstance();
+        Singleton sin2 = Singleton.getInstance();
+        Assertions.assertSame(sin1,sin2);
+    }
 
 }
